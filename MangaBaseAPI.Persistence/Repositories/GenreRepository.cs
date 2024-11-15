4c17402b5d41b91b893fa64e7c2a7982e7b0dea3
Nguyễn Thái Hòa <hoa41300@gmail.com>
2024-11-15 20:40:54 +0700

    Add genre and languaguecode repositories

---
+++ MangaBaseAPI.Persistence/Repositories/GenreRepository.cs
@@ -0,0 +1,12 @@
+﻿using MangaBaseAPI.Domain.Entities;
+using MangaBaseAPI.Domain.Repositories;
+
+namespace MangaBaseAPI.Persistence.Repositories
+{
+    public class GenreRepository : Repository<Genre>, IGenreRepository
+    {
+        public GenreRepository(MangaBaseDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ MangaBaseAPI.Persistence/Repositories/LanguageCodeRepository.cs
@@ -0,0 +1,12 @@
+﻿using MangaBaseAPI.Domain.Entities;
+using MangaBaseAPI.Domain.Repositories;
+
+namespace MangaBaseAPI.Persistence.Repositories
+{
+    public class LanguageCodeRepository : Repository<LanguageCode>, ILanguageCodeRepository
+    {
+        public LanguageCodeRepository(MangaBaseDbContext context) : base(context)
+        {
+        }
+    }
+}
