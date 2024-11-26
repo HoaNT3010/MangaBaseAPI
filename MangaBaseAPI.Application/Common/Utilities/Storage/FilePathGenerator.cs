0e160ed749d8628829c7c8a2b4a6ef28495ff145
Nguyễn Thái Hòa <hoa41300@gmail.com>
2024-11-26 15:46:36 +0700

    Add helper classes to aid file uploading process

---
+++ MangaBaseAPI.Application/Common/Utilities/Storage/FilePathGenerator.cs
@@ -0,0 +1,33 @@
+﻿namespace MangaBaseAPI.Application.Common.Utilities.Storage
+{
+    public static class FilePathGenerator
+    {
+        const string TitleFolderName = "titles";
+
+        public static string GenerateCoverImagePath(string titleId, string fileName)
+        {
+            return $"{TitleFolderName}/{titleId}/{fileName}";
+        }
+
+        public static string GenerateChapterImagePath(string titleId, string chapterIndex, string fileName)
+        {
+            return $"{TitleFolderName}/{titleId}/{chapterIndex}/{fileName}";
+        }
+
+        /// <summary>
+        /// Generate the folder path for storing images of the chapter. The path ends with forward slash '/'.
+        /// </summary>
+        /// <param name="titleId"></param>
+        /// <param name="chapterIndex"></param>
+        /// <returns></returns>
+        public static string GenerateChapterImageFolderPath(string titleId, string chapterIndex)
+        {
+            return $"{TitleFolderName}/{titleId}/{chapterIndex}/";
+        }
+
+        public static string GenerateFileUrl(string bucketUrl, string filePath)
+        {
+            return $"{bucketUrl}/{filePath}";
+        }
+    }
+}
---
+++ MangaBaseAPI.Application/Common/Utilities/Storage/ImageValidator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace MangaBaseAPI.Application.Common.Utilities.Storage
+{
+    public static class ImageValidator
+    {
+        static readonly string[] AllowedImageExtensions = { ".png", ".jpg", ".jpeg", ".webp" };
+        static readonly string[] AllowedMimeTypes = { "image/png", "image/jpeg", "image/webp" };
+
+        // 5MB
+        const long MaxFileSize = 5 * 1024 * 1024;
+
+        public static bool IsImageExtensionValid(string fileName)
+        {
+            return AllowedImageExtensions.Contains(Path.GetExtension(fileName).ToLower());
+        }
+
+        public static bool IsFileSizeValid(IFormFile file)
+        {
+            return file.Length <= MaxFileSize;
+        }
+
+        public static bool IsFileMimeTypeValid(IFormFile file)
+        {
+            return AllowedMimeTypes.Contains(file.ContentType.ToLower());
+        }
+    }
+}
