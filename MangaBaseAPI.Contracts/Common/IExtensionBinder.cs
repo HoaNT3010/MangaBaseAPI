06b534cada67e10dc7f61aad5996ded892a76941
Nguyễn Thái Hòa <hoa41300@gmail.com>
2024-12-25 08:20:01 +0700

    Add interface for processing complex query object.

---
+++ MangaBaseAPI.Contracts/Common/IExtensionBinder.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Reflection;
+
+namespace MangaBaseAPI.Contracts.Common
+{
+    public interface IExtensionBinder<T> where T : IExtensionBinder<T>
+    {
+        /// <summary>
+        /// The method discovered by RequestDelegateFactory on types used as parameters of route
+        /// handler delegates to support custom binding.
+        /// </summary>
+        /// <param name="httpContext">The <see cref="HttpContext"/>.</param>
+        /// <param name="parameter">The <see cref="ParameterInfo"/> for the parameter being bound to.</param>
+        /// <returns>The value to assign to the parameter.</returns>
+        static abstract ValueTask<T?> BindAsync(HttpContext httpContext, ParameterInfo parameter);
+    }
+}
--- MangaBaseAPI.Contracts/Creators/Search/SearchCreatorByNameRequest.cs
+++ MangaBaseAPI.Contracts/Creators/Search/SearchCreatorByNameRequest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MangaBaseAPI.Contracts.Common;
+using Microsoft.AspNetCore.Http;
 using System.Reflection;
 
 namespace MangaBaseAPI.Contracts.Creators.Search
@@ -6,13 +7,19 @@ namespace MangaBaseAPI.Contracts.Creators.Search
     public record SearchCreatorByNameRequest(
         string Keyword,
         int Page = 1,
-        int PageSize = 10)
+        int PageSize = 10) : IExtensionBinder<SearchCreatorByNameRequest>
     {
         public static ValueTask<SearchCreatorByNameRequest?> BindAsync(HttpContext httpContext, ParameterInfo parameter)
         {
             string keyword = httpContext.Request.Query["Keyword"];
-            int.TryParse(httpContext.Request.Query["Page"], out var page);
-            int.TryParse(httpContext.Request.Query["PageSize"], out var pageSize);
+            if (!int.TryParse(httpContext.Request.Query["Page"], out var page))
+            {
+                page = 1;
+            }
+            if (!int.TryParse(httpContext.Request.Query["PageSize"], out var pageSize))
+            {
+                pageSize = 10;
+            }
 
             var result = new SearchCreatorByNameRequest(keyword, page, pageSize);
 
