55132c8fc02e24bbe242f425f35f8987589315d4
Nguyễn Thái Hòa <hoa41300@gmail.com>
2025-01-08 00:56:15 +0700

    Update ChapterImage Entity
    Add property Index to entity ChapterImage and update table schema

--- MangaBaseAPI.Domain/Entities/ChapterImage.cs
+++ MangaBaseAPI.Domain/Entities/ChapterImage.cs
@@ -6,6 +6,7 @@ namespace MangaBaseAPI.Domain.Entities
     {
         public int Id { get; set; }
         public string Url { get; set; } = default!;
+        public int Index { get; set; }
 
         public Guid ChapterId { get; set; }
         public Chapter Chapter { get; set; } = default!;
---
+++ MangaBaseAPI.Persistence/Migrations/20250107175255_AddChapterImageIndex.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MangaBaseAPI.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddChapterImageIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Index",
+                table: "ChapterImages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "ConcurrencyStamp",
+                value: "04482017-67cd-41ac-b96c-b26173c74118");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Index",
+                table: "ChapterImages");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "ConcurrencyStamp",
+                value: "23d171cb-3b05-49ac-8427-e386af2aadf4");
+        }
+    }
+}
