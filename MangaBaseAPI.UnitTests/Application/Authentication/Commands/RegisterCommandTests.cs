30be6a298689c5358144a73e4b2cbf73303339d0
Nguyễn Thái Hòa <hoa41300@gmail.com>
2025-03-17 08:18:13 +0700

    Add Unit Tests For Authentication Features
    Add unit tests for features Register and Revoke Token in Authentication feature group.

---
+++ MangaBaseAPI.UnitTests/Application/Authentication/Commands/RegisterCommandTests.cs
@@ -0,0 +1,214 @@
+﻿using FluentAssertions;
+using MangaBaseAPI.Application.Authentication.Commands.Register;
+using MangaBaseAPI.CrossCuttingConcerns.BackgroundJob.HangfireScheduler;
+using MangaBaseAPI.CrossCuttingConcerns.Identity;
+using MangaBaseAPI.Domain.Constants.Role;
+using MangaBaseAPI.Domain.Entities;
+using MangaBaseAPI.Domain.Errors.Authentication;
+using MangaBaseAPI.Domain.Repositories;
+using MangaBaseAPI.UnitTests.Application.Helpers.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+
+namespace MangaBaseAPI.UnitTests.Application.Authentication.Commands
+{
+    public class RegisterCommandTests
+    {
+        private static readonly RegisterCommand Command = new(
+            "user-name",
+            "email",
+            "password",
+            "confirmed-password");
+
+        private readonly UserManager<User> _userManagerMock;
+        private readonly IPasswordHasher _passwordHasherMock;
+        private readonly IUnitOfWork _unitOfWorkMock;
+        private readonly ILogger<RegisterCommandHandler> _loggerMock;
+        private readonly IHangfireBackgroundJobService _jobServiceMock;
+        private readonly RegisterCommandHandler _handler;
+
+        public RegisterCommandTests()
+        {
+            _userManagerMock = MockUserManager.CreateMock<User>();
+            _passwordHasherMock = Substitute.For<IPasswordHasher>();
+            _unitOfWorkMock = Substitute.For<IUnitOfWork>();
+            _loggerMock = Substitute.For<ILogger<RegisterCommandHandler>>();
+            _jobServiceMock = Substitute.For<IHangfireBackgroundJobService>();
+            _handler = new RegisterCommandHandler(_userManagerMock,
+                _passwordHasherMock,
+                _unitOfWorkMock,
+                _loggerMock,
+                _jobServiceMock);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnError_WhenEmailNotUnique()
+        {
+            // Arrange
+            var invalidCommand = Command with { Email = "duplicated-email" };
+            _userManagerMock.FindByEmailAsync(invalidCommand.Email)
+                .Returns(Task.FromResult<User?>(new User()));
+
+            // Act
+            var result = await _handler.Handle(invalidCommand, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeFalse();
+            result.Error.Should().NotBeNull();
+            result.Error.Should().Be(RegisterErrors.EmailNotUnique);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnError_WhenUserNameNotUnique()
+        {
+            // Arrange
+            var invalidCommand = Command with { UserName = "duplicated-username" };
+            _userManagerMock.FindByEmailAsync(invalidCommand.Email)
+                .Returns(Task.FromResult<User?>(null));
+            _userManagerMock.FindByNameAsync(invalidCommand.UserName)
+                .Returns(Task.FromResult<User?>(new User()));
+
+            // Act
+            var result = await _handler.Handle(invalidCommand, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeFalse();
+            result.Error.Should().NotBeNull();
+            result.Error.Should().Be(RegisterErrors.UserNameNotUnique);
+        }
+
+        [Fact]
+        public async Task Handle_Should_CreateNewUser_WhenRegisterSuccessful()
+        {
+            // Arrange
+            _userManagerMock.FindByEmailAsync(Command.Email)
+                .Returns(Task.FromResult<User?>(null));
+            _userManagerMock.FindByNameAsync(Command.UserName)
+                .Returns(Task.FromResult<User?>(null));
+            User user = new User(Guid.NewGuid(),
+                Command.UserName,
+                Command.Email);
+            _passwordHasherMock.HashProvidedPassword(Arg.Any<User>(), Command.Password)
+                .Returns("hashed-password");
+            _unitOfWorkMock.BeginTransactionAsync(Arg.Any<CancellationToken>())
+                .Returns(Task.CompletedTask);
+            _userManagerMock.CreateAsync(Arg.Any<User>())
+                .Returns(Task.FromResult(IdentityResult.Success));
+            _userManagerMock.AddToRoleAsync(Arg.Any<User>(), ApplicationRoles.Member)
+                .Returns(Task.FromResult(IdentityResult.Success));
+            _unitOfWorkMock.CommitTransactionAsync(Arg.Any<CancellationToken>())
+                .Returns(Task.CompletedTask);
+
+            // Act
+            var result = await _handler.Handle(Command, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.Error.Should().BeNull();
+            await _userManagerMock.Received(1).CreateAsync(Arg.Any<User>());
+            await _userManagerMock.Received(1).AddToRoleAsync(Arg.Any<User>(), ApplicationRoles.Member);
+            await _unitOfWorkMock.Received(0).RollbackTransactionAsync(Arg.Any<CancellationToken>());
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnError_WhenFailedToCreateUser()
+        {
+            // Arrange
+            _userManagerMock.FindByEmailAsync(Command.Email)
+                .Returns(Task.FromResult<User?>(null));
+            _userManagerMock.FindByNameAsync(Command.UserName)
+                .Returns(Task.FromResult<User?>(null));
+            User user = new User(Guid.NewGuid(),
+                Command.UserName,
+                Command.Email);
+            _passwordHasherMock.HashProvidedPassword(Arg.Any<User>(), Command.Password)
+                .Returns("hashed-password");
+            _unitOfWorkMock.BeginTransactionAsync(Arg.Any<CancellationToken>())
+                .Returns(Task.CompletedTask);
+            _userManagerMock.CreateAsync(Arg.Any<User>())
+                .Returns(Task.FromResult(IdentityResult.Failed()));
+
+            // Act
+            var result = await _handler.Handle(Command, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeFalse();
+            result.Error.Should().NotBeNull();
+            result.Error.Should().Be(RegisterErrors.CreateUserFailed);
+            await _userManagerMock.Received(1).CreateAsync(Arg.Any<User>());
+            await _userManagerMock.Received(0).AddToRoleAsync(Arg.Any<User>(), ApplicationRoles.Member);
+            await _unitOfWorkMock.Received(1).RollbackTransactionAsync(Arg.Any<CancellationToken>());
+            await _unitOfWorkMock.Received(0).CommitTransactionAsync(Arg.Any<CancellationToken>());
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnError_WhenFailedToCreateUserRole()
+        {
+            // Arrange
+            _userManagerMock.FindByEmailAsync(Command.Email)
+                .Returns(Task.FromResult<User?>(null));
+            _userManagerMock.FindByNameAsync(Command.UserName)
+                .Returns(Task.FromResult<User?>(null));
+            User user = new User(Guid.NewGuid(),
+                Command.UserName,
+                Command.Email);
+            _passwordHasherMock.HashProvidedPassword(Arg.Any<User>(), Command.Password)
+                .Returns("hashed-password");
+            _unitOfWorkMock.BeginTransactionAsync(Arg.Any<CancellationToken>())
+                .Returns(Task.CompletedTask);
+            _userManagerMock.CreateAsync(Arg.Any<User>())
+                .Returns(Task.FromResult(IdentityResult.Success));
+            _userManagerMock.AddToRoleAsync(Arg.Any<User>(), ApplicationRoles.Member)
+                .Returns(Task.FromResult(IdentityResult.Failed()));
+
+            // Act
+            var result = await _handler.Handle(Command, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeFalse();
+            result.Error.Should().NotBeNull();
+            result.Error.Should().Be(RegisterErrors.AssignUserRoleFailed);
+            await _userManagerMock.Received(1).CreateAsync(Arg.Any<User>());
+            await _userManagerMock.Received(1).AddToRoleAsync(Arg.Any<User>(), ApplicationRoles.Member);
+            await _unitOfWorkMock.Received(1).RollbackTransactionAsync(Arg.Any<CancellationToken>());
+            await _unitOfWorkMock.Received(0).CommitTransactionAsync(Arg.Any<CancellationToken>());
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnError_WhenFailedToCommitTransaction()
+        {
+            // Arrange
+            _userManagerMock.FindByEmailAsync(Command.Email)
+                .Returns(Task.FromResult<User?>(null));
+            _userManagerMock.FindByNameAsync(Command.UserName)
+                .Returns(Task.FromResult<User?>(null));
+            User user = new User(Guid.NewGuid(),
+                Command.UserName,
+                Command.Email);
+            _passwordHasherMock.HashProvidedPassword(Arg.Any<User>(), Command.Password)
+                .Returns("hashed-password");
+            _unitOfWorkMock.BeginTransactionAsync(Arg.Any<CancellationToken>())
+                .Returns(Task.CompletedTask);
+            _userManagerMock.CreateAsync(Arg.Any<User>())
+                .Returns(Task.FromResult(IdentityResult.Success));
+            _userManagerMock.AddToRoleAsync(Arg.Any<User>(), ApplicationRoles.Member)
+                .Returns(Task.FromResult(IdentityResult.Success));
+            _unitOfWorkMock.CommitTransactionAsync(Arg.Any<CancellationToken>())
+                .ThrowsAsync(new Exception("Failed to commit database transaction"));
+
+            // Act
+            var result = await _handler.Handle(Command, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeFalse();
+            result.Error.Should().NotBeNull();
+            result.Error.Should().Be(RegisterErrors.UnexpectedError);
+            await _userManagerMock.Received(1).CreateAsync(Arg.Any<User>());
+            await _userManagerMock.Received(1).AddToRoleAsync(Arg.Any<User>(), ApplicationRoles.Member);
+            await _unitOfWorkMock.Received(1).RollbackTransactionAsync(Arg.Any<CancellationToken>());
+            await _unitOfWorkMock.Received(1).CommitTransactionAsync(Arg.Any<CancellationToken>());
+        }
+    }
+}
---
+++ MangaBaseAPI.UnitTests/Application/Authentication/Commands/RevokeTokenCommandTests.cs
@@ -0,0 +1,121 @@
+﻿using FluentAssertions;
+using MangaBaseAPI.Application.Authentication.Commands.RevokeToken;
+using MangaBaseAPI.CrossCuttingConcerns.Jwt;
+using MangaBaseAPI.Domain.Errors.Authentication;
+using MangaBaseAPI.Domain.Repositories;
+using NSubstitute;
+
+namespace MangaBaseAPI.UnitTests.Application.Authentication.Commands
+{
+    public class RevokeTokenCommandTests
+    {
+        private static readonly RevokeTokenCommand Command = new("valid-refresh-token");
+
+        private readonly IJwtTokenProvider _jwtTokenProviderMock;
+        private readonly IUnitOfWork _unitOfWorkMock;
+        private readonly IUserTokenRepository _userTokenRepoMock;
+        private readonly RevokeTokenCommandHandler _handler;
+
+        public RevokeTokenCommandTests()
+        {
+            _jwtTokenProviderMock = Substitute.For<IJwtTokenProvider>();
+            _unitOfWorkMock = Substitute.For<IUnitOfWork>();
+            _userTokenRepoMock = Substitute.For<IUserTokenRepository>();
+            _handler = new RevokeTokenCommandHandler(_jwtTokenProviderMock, _unitOfWorkMock);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnError_WhenTokenIsExpired()
+        {
+            // Arrange
+            var invalidCommand = new RevokeTokenCommand("expired-refresh-token");
+            _jwtTokenProviderMock.IsTokenInvalidOrExpired(invalidCommand.refreshToken)
+                .Returns(true);
+
+            // Act
+            var result = await _handler.Handle(invalidCommand, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeFalse();
+            result.Error.Should().NotBeNull();
+            result.Error.Should().Be(RefreshTokenErrors.InvalidOrExpiredToken);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnError_WhenCannotFindAndRemoveTokenByValue()
+        {
+            // Arrange
+            var invalidCommand = new RevokeTokenCommand("invalid-refresh-token");
+            _jwtTokenProviderMock.IsTokenInvalidOrExpired(invalidCommand.refreshToken)
+                .Returns(false);
+            _unitOfWorkMock.GetRepository<IUserTokenRepository>().Returns(_userTokenRepoMock);
+            _userTokenRepoMock.TryRemoveTokenByValueAsync(invalidCommand.refreshToken, CancellationToken.None)
+                .Returns(false);
+
+            // Act
+            var result = await _handler.Handle(invalidCommand, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeFalse();
+            result.Error.Should().NotBeNull();
+            result.Error.Should().Be(RefreshTokenErrors.TokenEntryNotFound);
+        }
+
+        [Fact]
+        public async Task Handle_Should_ReturnError_WhenUpdateTokenFailed()
+        {
+            // Arrange
+            _jwtTokenProviderMock.IsTokenInvalidOrExpired(Command.refreshToken)
+                .Returns(false);
+            _unitOfWorkMock.GetRepository<IUserTokenRepository>().Returns(_userTokenRepoMock);
+            _userTokenRepoMock.TryRemoveTokenByValueAsync(Command.refreshToken, CancellationToken.None)
+                .Returns(true);
+            _unitOfWorkMock.SaveChangeAsync(CancellationToken.None).Returns(0);
+
+            // Act
+            var result = await _handler.Handle(Command, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeFalse();
+            result.Error.Should().NotBeNull();
+            result.Error.Should().Be(RefreshTokenErrors.UpdateRefreshTokenFailed);
+        }
+
+        [Fact]
+        public async Task Handle_Should_CallSaveChange_WhenTokenIsValid()
+        {
+            // Arrange
+            _jwtTokenProviderMock.IsTokenInvalidOrExpired(Command.refreshToken)
+                .Returns(false);
+            _unitOfWorkMock.GetRepository<IUserTokenRepository>().Returns(_userTokenRepoMock);
+            _userTokenRepoMock.TryRemoveTokenByValueAsync(Command.refreshToken, CancellationToken.None)
+                .Returns(true);
+
+            // Act
+            var result = await _handler.Handle(Command, CancellationToken.None);
+
+            // Assert
+            await _unitOfWorkMock.Received(1).SaveChangeAsync(Arg.Any<CancellationToken>());
+        }
+
+        [Fact]
+        public async Task Handle_Should_RevokeToken_WhenUpdateTokenSuccessful()
+        {
+            // Arrange
+            _jwtTokenProviderMock.IsTokenInvalidOrExpired(Command.refreshToken)
+                .Returns(false);
+            _unitOfWorkMock.GetRepository<IUserTokenRepository>().Returns(_userTokenRepoMock);
+            _userTokenRepoMock.TryRemoveTokenByValueAsync(Command.refreshToken, CancellationToken.None)
+                .Returns(true);
+            _unitOfWorkMock.SaveChangeAsync(CancellationToken.None).Returns(1);
+
+            // Act
+            var result = await _handler.Handle(Command, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.Error.Should().BeNull();
+            result.Value.message.Should().Be("Token revoked successfully");
+        }
+    }
+}
